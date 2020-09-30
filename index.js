a4ecd8e5361357d852cc35df7a9aff5b1c249316
Wes <wmcmillan1012@gmail.com>
2020-09-30 14:48:18 -0700

    added more pseudo code+ some basic functions

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 //require all the dependencies I'll need- inquirer, fs
 
 //create an array of questions to ask the user
+const questions = [
+
+];
 
 //write a README in a markdown file to generate a template
     //then copy/paste that template
@@ -15,3 +18,14 @@
 //use answers that come back from inquirer - pass those into my generate readme function
 
 //write file using template generated from readme function
+function writeToFile(fileName, data) {
+    
+}
+
+//function to initialize program
+function init() {
+
+}
+
+//function call to initialize program
+init();
