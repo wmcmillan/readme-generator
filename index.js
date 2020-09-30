250d4d439b3803554598021e45e324dd07e19d0b
Wes <wmcmillan1012@gmail.com>
2020-09-30 14:42:51 -0700

    added pseudo code and copied instructional readme

---
+++ index.js
@@ -0,0 +1,17 @@
+//npm initialization
+//install my dependencies- inquirer
+
+//require all the dependencies I'll need- inquirer, fs
+
+//create an array of questions to ask the user
+
+//write a README in a markdown file to generate a template
+    //then copy/paste that template
+
+//function that will generate my readme template
+
+//use inquirer to prompt users with questions
+
+//use answers that come back from inquirer - pass those into my generate readme function
+
+//write file using template generated from readme function
